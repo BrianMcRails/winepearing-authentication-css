92be1c8aa247e2705b3382e430bffdfba6071edf
Brian D. McAuley <brian.mcauley9@gmail.com>
2016-02-13 18:37:45 -0800

    added validation to address entry

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ActiveRecord::Base
   belongs_to :user
+  validates :address_line_1, presence: true
+  validates :address_city, presence: true
+  validates :address_state, presence: true
+  validates :address_zip, presence: true
+  validates :recipient_address_line_1, presence: true
+  validates :recipient_address_city, presence: true
+  validates :recipient_address_state, presence: true
+  validates :recipient_address_zip, presence: true
 end
